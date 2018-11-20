d88d7c1886cf6a387b41906c337cdd12d27ea00c
Floflow <floperrindb@gmail.com>
2018-11-20 17:32:34 +0000

    searchbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "13Ad2mtKsGukPC"
+    };
+    this.search("walt disney");
+  }
+
+  search = (query) => {
+    giphy().search({
+      q: query,
+      rating: 'g'
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data,
+      });
+    });
+  }
+
   render(){
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction = {this.search}/>
           <div className="selected-gif">
-            <Gif id="1UPSV8da9qG3qOvhk2" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-
+          <div className="gif-list">
+            <GifList gifs={this.state.gifs} />
+          </div>
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render(){
-    const src = "https://media.giphy.com/media/`${this.props.id}`/giphy.gif";
+    const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
       <img src= {src} className="gif"/>
     );
